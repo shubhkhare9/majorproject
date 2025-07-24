6a4bd4ac76eeeaac5c22a86b69e4b66f809d017b
Shubh Khare <shubhkhare.sk11@gmail.com>
2025-07-24 17:28:37 +0530

    Update settings.py

--- majorproject/settings.py
+++ majorproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$=-cyjx-i0o*l=zg)rtq-3_2(3)uh#ui#892vjiby9=injc68o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
